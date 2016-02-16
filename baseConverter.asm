049d0d20c10efa46e42a400c4bd6aaa9be87b608
Matheus Teixeira <matheus.mtxr@gmail.com>
2016-02-15 23:33:19 -0200

    Seg Fev 15 23:33:19 BRST 2016

--- baseConverter.asm
+++ baseConverter.asm
@@ -1,4 +1,5 @@
-# Start .data segment (data!)
+# created by Matheus Martins Teixeira <me@mteixeira.com.br>
+# USP Id 7277482
 
 # syscall used from
 # http://courses.missouristate.edu/KenVollmar/mars/Help/SyscallHelp.html
@@ -10,19 +11,18 @@
     inputNumberText:    .asciiz "Digite o numero a ser convertido:   "
     ivalidBaseText:     .asciiz "Base invalida fornecida."
     outputText:         .asciiz "O numero na nova base eh: "
-    inputNumberArray:   .space 32
-    outputNumberArray:  .space 32
+    inputNumberArray:   .space  32
+    outputNumberArray:  .space  32
     newline:            .asciiz "\n"
-    binary:             .byte 'b'
-    octal:              .byte 'o'
-    decimal:            .byte 'd'
-    hexa:               .byte 'h'
+    binary:             .byte   'b'
+    octal:              .byte   'o'
+    decimal:            .byte   'd'
+    hexa:               .byte   'h'
 
 
 # program code
 .text
 
-    .globl  main
 main:
     # Print string inputBaseText
     la   $a0, inputBaseText     # load the address of inputBaseText
@@ -30,7 +30,7 @@ main:
 
     # Get input base from user and save
     jal  readBase
-    move $t1, $v0
+    move $t0, $v0
     jal  printNewline
     ############# END READING INPUT BASE #######################################
 
@@ -42,7 +42,6 @@ main:
     jal  printString
 
     # Get input number from user and save
-    la   $a0, inputNumberArray  # load inputBase address to argument0
     jal  readNumber
     jal  printNewline
     ############# END READING INPUT NUMBER #######################################
@@ -56,13 +55,13 @@ main:
 
     # Get output base from user and save
     jal  readBase
-    move $t2, $v0
+    move $t1, $v0
     jal  printNewline
     ############# END READING OUTPUT BASE #######################################
 
     # At this point, we have:
-    # t1 = base from conversion
-    # t2 = base to conversion
+    # t0 = base from conversion
+    # t1 = base to conversion
 
     j convert
 
@@ -80,25 +79,31 @@ printOutput:
     j exit
 
 convert:
+    # t9 will be used as an auxiliary var for comparisions
+    # t8 will be our counter during conversion
+    # t2 will receive inputNumberArray address
+    # t7 will be current number to convert
+    #
+
     # is input binary?
-    la $t9, binary
-    lb $t9, 0($t9)
-    beq $t9, $t1, convertFromBinary
+    la   $t9, binary
+    lb   $t9, 0($t9)
+    beq  $t9, $t0, convertFromBinary
 
     # is input octal?
-    la $t9, octal
-    lb $t9, 0($t9)
-    beq $t9, $t1, convertFromOctal
+    la   $t9, octal
+    lb   $t9, 0($t9)
+    beq  $t9, $t0, convertFromOctal
 
     # is input Decimal?
-    la $t9, decimal
-    lb $t9, 0($t9)
-    beq $t9, $t1, convertFromDecimal
+    la   $t9, decimal
+    lb   $t9, 0($t9)
+    beq  $t9, $t0, convertFromDecimal
 
     # is input Hexa?
-    la $t9, hexa
-    lb $t9, 0($t9)
-    beq $t9, $t1, convertFromHexa
+    la   $t9, hexa
+    lb   $t9, 0($t9)
+    beq  $t9, $t0, convertFromHexa
 
     # input is hexa
     j invalidBase
@@ -106,28 +111,62 @@ convert:
 
 convertFromBinary:
     # output base is binary too, so just print it
-    la $t9, binary
-    lb $t9, 0($t9)
-    beq $t9, $t2, printOutput
+    la   $t9, binary
+    lb   $t9, 0($t9)
+    beq  $t9, $t1, printOutput
+
+    la   $t9, decimal
+    lb   $t9, 0($t9)
+    beq  $t9, $t1, convertFromBinaryToDecimal
+
+convertFromBinaryToDecimal:
+    # start counter
+    la   $t2, inputNumberArray       # load inputNumber address to t2
+    li   $t8, 1                      # start our counter
+    li   $a0, 0                      # output number
+    j binaryToDecimalLoop
+
+binaryToDecimalLoop:
+    lb   $t7, 0($t2)
+    addi $t7, $t7, -48              # convert from string to int
+    blt  $t7, $zero, printInt       # print int if t7 < 0
+    mul  $t7, $t7, $t8              # mult t7 * t8
+    add  $a0, $a0, $t7              # add t7 to a0
+    li   $t6, 2                     # load 2 to t6
+    mul  $t8, $t8, $t6              # t8 = t8 * t6
+    addi $t2, $t2, 1                # increment array position
+    li   $t7, 32                    # t7 = 32
+    bgt  $t8, $t7, printInt         # print int if t8 > t7 (or 32)
+    j binaryToDecimalLoop
+
+printInt:
+    move $a1, $a0
+    la   $a0, outputText
+    jal  printString
+
+    move $a0, $a1
+    li  $v0, 1          # print_string syscall code = 4
+    syscall
+    j exit
 
 
 convertFromOctal:
     # output base is octal too, so just print it
-    la $t9, octal
-    lb $t9, 0($t9)
-    beq $t9, $t2, printOutput
+    la   $t9, octal
+    lb   $t9, 0($t9)
+    beq  $t9, $t1, printOutput
 
 convertFromDecimal:
     # output base is decimal too, so just print it
-    la $t9, decimal
-    lb $t9, 0($t9)
-    beq $t9, $t2, printOutput
+    la   $t9, decimal
+    lb   $t9, 0($t9)
+    beq  $t9, $t1, printOutput
 
 convertFromHexa:
     # output base is hexa too, so just print it
-    la $t9, hexa
-    lb $t9, 0($t9)
-    beq $t9, $t2, printOutput
+    la   $t9, hexa
+    lb   $t9, 0($t9)
+    beq  $t9, $t1, printOutput
 
 invalidBase:
     la   $a0, invalidBase
@@ -146,17 +185,18 @@ printString:
     jr  $ra
 
 readBase:
-    li  $v0, 12          # read_string syscall code = 8
+    li  $v0, 12         # read_string syscall code = 8
     syscall
     jr  $ra
 
 readNumber:
-    li  $v0, 8          # read_string syscall code = 8
-    li  $a1, 32         # space allocated for inputBase
+    la   $a0, inputNumberArray  # load inputBase address to argument0
+    li   $v0, 8          # read_string syscall code = 8
+    li   $a1, 32         # space allocated for inputBase
     syscall
-    jr  $ra
+    jr   $ra
 
 exit:
     jal printNewline
-    li  $v0, 10         # exit
+    li   $v0, 10         # exit
     syscall
