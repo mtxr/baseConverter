bc931694f7be04f9f4cd0578887886a57ad57a56
Matheus Teixeira <matheus.mtxr@gmail.com>
2016-02-16 20:33:21 -0200

    Ter Fev 16 20:33:21 BRST 2016

--- baseTo.asm
+++ baseTo.asm
@@ -4,25 +4,27 @@
 convertToBinary:
     li   $t0, 2             # load t0 with 2 to divide
     la   $a1, auxiliaryArray # load outputArrayAddress
-    j    decimalToBinaryLoop
-
-decimalToBinaryLoop:
-    divu $a0, $t0            # lo = a0/2, hi = a0 % 2
-    mfhi $t1                 # t1 = hi (remainder)
-    mflo $a0                 # a0 = lo or a0 = (a0/2)
-    addi $t1, $t1, 48        # convert to char
-    sb   $t1, 0($a1)         # save to outputArray
-    addi $a1, $a1, 1         # ++ auxiliaryArray position
-    bgtz $a0, decimalToBinaryLoop
-    jal  revertToOutputArray
-    j    outputAsString
-
-# @TODO: FINISH IMPLEMENTATION
+    j    decimalToT0ValueBase
 
 # DECIMAL -> OCTAL STRING ARRAY
 convertToOctal:
-# @TODO: FINISH IMPLEMENTATION
+    li   $t0, 8              # load t0 with 8 to divide
+    la   $a1, auxiliaryArray # load outputArrayAddress
+    j    decimalToT0ValueBase
+
 
 # DECIMAL -> HEXA STRING ARRAY
 convertToHexa:
 # @TODO: FINISH IMPLEMENTATION
+
+
+decimalToT0ValueBase:        # receive t0 with the divisor value
+    divu $a0, $t0            # lo = a0/t0, hi = a0 % t0
+    mfhi $t1                 # t1 = hi (remainder)
+    mflo $a0                 # a0 = lo or a0 = (a0/t0)
+    addi $t1, $t1, 48        # convert to char
+    sb   $t1, 0($a1)         # save to outputArray
+    addi $a1, $a1, 1         # ++ auxiliaryArray position
+    bgtz $a0, decimalToT0ValueBase
+    jal  revertToOutputArray
+    j    outputAsString
